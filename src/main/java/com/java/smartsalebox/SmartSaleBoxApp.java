1163e1f5468d77b0626d77ba97991e1a48f06d52
Armando Perea <terry900407@gmail.com>
2020-10-29 12:11:57 -0600

    Ready to begin model section

---
+++ src/main/java/com/java/smartsalebox/SmartSaleBoxApp.java
@@ -0,0 +1,16 @@
+package com.java.smartsalebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello SmartSaleBoxApp!
+ *
+ */
+@SpringBootApplication
+public class SmartSaleBoxApp {
+	
+	public static void main( String[] args ){
+        SpringApplication.run(SmartSaleBoxApp.class, args);
+    }
+}

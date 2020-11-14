dc6e595db6e0cec3975a044afbbba2d880b2d1a7
Armando Perea <terry900407@gmail.com>
2020-11-14 14:22:41 -0600

    Repository bean added

--- src/main/java/com/java/smartsalebox/repo/impl/ControlBulkStockRepoImpl.java
+++ src/main/java/com/java/smartsalebox/repo/impl/ControlBulkStockRepoImpl.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.java.smartsalebox.models.ControlBulkStock;
 import com.java.smartsalebox.repository.ControlBulkStockRepository;
 
+@Repository
 public class ControlBulkStockRepoImpl {
 
 
